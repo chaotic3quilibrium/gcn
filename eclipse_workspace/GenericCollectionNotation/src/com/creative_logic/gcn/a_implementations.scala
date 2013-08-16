3d0456da89f0a6f1c490d47231590507506c4124
Jim O'Flaherty <jim.oflaherty.jr@gmail.com>
2013-08-16 18:31:51 -0500

    Refactored to improve interface consistency, add canEquals, equals and hashCode and substantially reduce code logic by making UnitToBaseAssociationImpl take two BaseGroup instances for its constructor. Also refactored the instance factories to casade their calls to a single factory method which does the only concrete instantiation.

--- eclipse_workspace/GenericCollectionNotation/src/com/creative_logic/gcn/a_implementations.scala
+++ eclipse_workspace/GenericCollectionNotation/src/com/creative_logic/gcn/a_implementations.scala
@@ -6,77 +6,96 @@ import scala.collection.immutable.ListSet
 
 object Implementations {
   final object UnitToBaseAssociation {
-    def createWithMap(keyValues: Map[Unit, Base], valuesHasEmptys: Boolean = false): UnitToBaseAssociation = 
-      if (!keyValues.isEmpty) new UnitToBaseAssociationImpl(ListMap[Unit, Base]() ++ keyValues, false, valuesHasEmptys)
+    def createWithMap(keyValues: Map[Unit, Base]): UnitToBaseAssociation = 
+      if (!keyValues.isEmpty) {
+        val (keys, values) = keyValues.unzip
+        createWithParallelBaseGroups(BaseGroup.createWithSeq(keys.toSeq), BaseGroup.createWithSeq(values.toSeq))
+      }
       else UnitToBaseAssociationEmpty
-    def createWithListMap(keyValues: ListMap[Unit, Base], valuesHasEmptys: Boolean = false): UnitToBaseAssociation = 
-      if (!keyValues.isEmpty) new UnitToBaseAssociationImpl(keyValues, true, valuesHasEmptys)
+    def createWithListMap(keyValues: ListMap[Unit, Base]): UnitToBaseAssociation = 
+      if (!keyValues.isEmpty) {
+        val (keys, values) = keyValues.unzip
+        createWithParallelBaseGroups(BaseGroup.createWithSeq(keys.toSeq), BaseGroup.createWithSeq(values.toSeq), true)
+      }
+      else UnitToBaseAssociationEmpty
+    def createWithParallelBaseGroups(keys: BaseGroup, values: BaseGroup, keySetIsInsertOrdered: Boolean = false): UnitToBaseAssociation =
+      if (!keys.isEmpty) new UnitToBaseAssociationImpl(keys, values, keySetIsInsertOrdered)
       else UnitToBaseAssociationEmpty
   }
   private final object UnitToBaseAssociationEmpty extends UnitToBaseAssociation {
-    final override val kindShallow = kind
-    final override val isEmpty = true
     final override val content = ListMap.empty[Unit, Base]
+    final override val isInsertOrdered = false
+    final override val kindShallow = Kind.Base
+    final override val isEmpty = true
+    final override val hasEmptys = false
+    final override val hasDuplicates = false
+    final override val isImpureShallow = false
+    final override val isImpureDeep = false
   }
-  private final class UnitToBaseAssociationImpl(keyValues: ListMap[Unit, Base], keySetIsInsertOrdered: Boolean, valuesHasEmptys: Boolean) extends UnitToBaseAssociation {
-    require(!keyValues.isEmpty, "keyValues must not be empty")
-    require(!keyValues.keySet.exists(_.isEmpty), "keyValues.keySet must not contain any instances of GcnValue.isEmpty")
-    require(valuesHasEmptys || !keyValues.values.exists(_.isEmpty), "when valuesHasNones is false, keyValues.values must not contain any instances where GcnObject.isEmpty is true")
+  private final class UnitToBaseAssociationImpl(keys: BaseGroup, values: BaseGroup, keySetIsInsertOrdered: Boolean = false) extends UnitToBaseAssociation {
+    require(keys.canBeUnitToBaseAssociationKeySet, "keys.canBeUnitToBaseAssociationKeySet must be true")
+    require(!values.isEmpty, "values must not be empty")
+    require(keys.content.length == values.content.length, "keys.content.length [" + keys.content.length + "] must be equal to values.content.length [" + values.content.length + "]")
+    final override val content = keys.content.map(_.asInstanceOf[Unit]).zip(values.content).toMap
     final override val isInsertOrdered = keySetIsInsertOrdered
-    final override val hasEmptys = valuesHasEmptys
-    final override val isImpureShallow = {
-      val kind = keyValues.head._2.kind
-      content.exists(_._2.kind != kind)
-    }
-    final override val isImpureDeep = content.exists(_._2.isImpureDeep)
-    final override val kindShallow = if (isImpureShallow) Kind.Base else keyValues.head._2.kind
-    final override val content = keyValues;
+    final override val kindShallow = values.kindShallow
+    final override val isEmpty = values.isEmpty               //should always be false given values.content.length > 0
+    final override val hasEmptys = values.hasEmptys
+    final override val hasDuplicates = values.hasDuplicates 
+    final override val isImpureShallow = values.isImpureShallow 
+    final override val isImpureDeep = values.isImpureDeep 
   }
   
   final object BaseGroup {
-    def createWithSet(values: Set[Base], valuesHasEmptys: Boolean = false): BaseGroup =
-      if (!values.isEmpty) new BaseGroupImpl(values.toSeq, false, false, valuesHasEmptys)
+    def createWithSet(values: Set[Base]): BaseGroup =
+      if (!values.isEmpty) createWithSeq(values.toSeq)
       else BaseGroupEmpty
-    def createWithSortedSet(values: ListSet[Base], valuesHasEmptys: Boolean = false): BaseGroup =
-      if (!values.isEmpty) new BaseGroupImpl(values.toSeq, true, false, valuesHasEmptys)
+    def createWithListSet(values: ListSet[Base]): BaseGroup =
+      if (!values.isEmpty) createWithSeq(values.toSeq, true)
       else BaseGroupEmpty
-    def createWithSeq(values: Seq[Base], valuesIsOrdered: Boolean = false, valuesHasDuplicates: Boolean = false, valuesHasEmptys: Boolean = false): BaseGroup =
-      if (!values.isEmpty) new BaseGroupImpl(values, valuesIsOrdered, valuesHasDuplicates, valuesHasEmptys)
+    def createWithSeq(values: Seq[Base], valuesIsOrdered: Boolean = false): BaseGroup =
+      if (!values.isEmpty) new BaseGroupImpl(values, valuesIsOrdered)
       else BaseGroupEmpty
   }
   private final object BaseGroupEmpty extends BaseGroup {
-    final override val kindShallow = kind
-    final override val isEmpty = true
     final override val content = Seq.empty[Base]
+    final override val isInsertOrdered = false
+    final override val kindShallow = Kind.Base
+    final override val isEmpty = true
+    final override val hasEmptys = false
+    final override val hasDuplicates = false
+    final override val isImpureShallow = false
+    final override val isImpureDeep = false
     final override lazy val toUnitToBaseAssociation: UnitToBaseAssociation = UnitToBaseAssociationEmpty
   }
-  private final class BaseGroupImpl(values: Seq[Base], valuesIsInsertOrdered: Boolean, valuesHasDuplicates: Boolean, valuesHasEmptys: Boolean) extends BaseGroup {
+  private final class BaseGroupImpl(values: Seq[Base], valuesIsInsertOrdered: Boolean) extends BaseGroup {
     require(!values.isEmpty, "values must not be empty")
-    require(valuesHasDuplicates || (values == values.distinct), " if (valuesHasDuplicates == false), values must be distinct")
+    final override val content = values;
     final override val isInsertOrdered = valuesIsInsertOrdered
-    final override val hasDuplicates = valuesHasDuplicates
-    final override val hasEmptys = valuesHasEmptys
+    final override val kindShallow = if (isImpureShallow) Kind.Base else values.head.kind
+    final override val isEmpty = false
+    final override val hasEmptys = values.exists(_.isEmpty)
+    final override val hasDuplicates = values.length > values.distinct.length
     final override val isImpureShallow = {
       val kind = values.head.kind
       content.exists(_.kind != kind)
     }
     final override val isImpureDeep = content.exists(_.isImpureDeep)
-    final override val kindShallow = if (isImpureShallow) Kind.Base else values.head.kind
-    final override val content = values;
     final override lazy val toUnitToBaseAssociation: UnitToBaseAssociation = {
-      val x = content.zipWithIndex.map(x => (UnitString(x._2.toString), x._1))
-      UnitToBaseAssociation.createWithListMap(ListMap[Unit, Base]() ++ x, false)
+      val x = content.zipWithIndex.map(x => (new UnitString(x._2.toString), x._1))
+      UnitToBaseAssociation.createWithListMap(ListMap[Unit, Base]() ++ x)
     }
   }
 
-  abstract class UnitImpl(val content: String) extends Unit {
-    final override lazy val toBaseGroup: BaseGroup = BaseGroup.createWithSeq(List(this), true, false, false)
+  abstract class UnitImpl(rawValue: String) extends Unit {
+    final override val content = rawValue
+    final override val isEmpty = rawValue.length() == 0
+    final override lazy val toBaseGroup: BaseGroup = BaseGroup.createWithSeq(List(this))
   }
-  final case class UnitString(rawValue: String) extends UnitImpl(rawValue) {
-    final override val isEmpty = rawValue.length() > 0
+  final class UnitString(rawValue: String) extends UnitImpl(rawValue) {
     final override val concreteType = UnitType.String
   }
-  private case class UnitIntegerSigned32(val rawValue: Int) extends UnitImpl(rawValue.toString) {
+  private class UnitIntegerSigned32(val rawValue: Int) extends UnitImpl(rawValue.toString) {
     final override val concreteType = UnitType.IntegerSigned32
   }
   //TODO: fill in the remaining UnitTypes implementations; Boolean, Trinary, Identifier, etc. 
--- eclipse_workspace/GenericCollectionNotation/src/com/creative_logic/gcn/a_interfaces.scala
+++ eclipse_workspace/GenericCollectionNotation/src/com/creative_logic/gcn/a_interfaces.scala
@@ -1,11 +1,9 @@
 package com.creative_logic.gcn
 
-import scala.collection.immutable.ListMap
-
 object Interfaces {
   object Kind extends Enumeration {
     type Kind = Value
-    val Base =                  Value("Base") //exactly zero instances of this exist - used as root object for "collections" containing more then one of the non-abstract types 
+    val Base =                  Value("Base") //exactly zero instances of this exist - used as root interface for "collections" containing more then one of the non-abstract types 
     val UnitToBaseAssociation = Value("UnitToBaseAssociation")
     val BaseGroup =             Value("BaseGroup")
     val Unit =                  Value("Unit")
@@ -13,56 +11,81 @@ object Interfaces {
     def isAbstract(kindValue: Kind.Value): Boolean = kindValue.id == Base.id
   }
   
-  trait BaseCharacteristics {
-    val isInsertOrdered: Boolean = false //defaults to assuming insertion ordering is not significant; i.e. Java Set/Map.keySet()
-    val hasDuplicates: Boolean = false   //defaults to an array not allowing duplicates; i.e. Java Set
-    val hasEmptys: Boolean = false       //defaults to not allowing values with isEmpty
-    val isImpureShallow: Boolean = false //defaults to assuming members are uniform at surface; same base class (excluding OBJECT)
-    val isImpureDeep: Boolean = false    //defaults to assuming members are uniform from surface to every leaf; same base class (excluding OBJECT)
-    val kindShallow: Kind.Value
+  trait Base {                                           //exactly zero instances of this exist
+    def canEqual(other: Any) = other.isInstanceOf[Base]  //should always be false unless overridden in descendant
+    override def equals(other: Any): Boolean =
+      other match {
+        case that: Base => (
+                 that.canEqual(this)
+              && (kind == that.kind)  
+              && (content == that.content)
+            )
+        case _ => false
+    }
+    override def hashCode = 41 * (41 * (41 + kind.hashCode)) + content.hashCode
+    val kind: Kind.Value                                 //constructor parameter: must be of Intefaces.Kind where !Kind.isAbstract for kind
+    def content: Any                                     //constructor parameter: 
+    def isInsertOrdered: Boolean                         //constructor parameter: false if content ordering not significant
+    def kindShallow: Kind.Value                          //content evaluation: base type of the values present in the content
+    def isEmpty: Boolean                                 //content evaluation: false if any content present
+    def hasEmptys: Boolean                               //content evaluation: false if all contained Base.isEmpty is false
+    def hasDuplicates: Boolean                           //content evaluation: false if all contained Base is not equal to any other contained Base
+    def isImpureShallow: Boolean                         //content evaluation: false if all contained Base.kind are equal
+    def isImpureDeep: Boolean                            //content evaluation: false if all contained Base.content's Base are equal
   }
   
-  trait Base extends BaseCharacteristics {
-    val kind: Kind.Value                 //exactly zero instances of this exist
-    val isEmpty: Boolean = false
-  }
-
   trait UnitToBaseAssociation extends Base {
-    final override val kind = Kind.UnitToBaseAssociation     //defaults to behave like an immutable Java HashMap<GcnValue, GcnObject>
-    val content: ListMap[Unit, Base]
+    final override def canEqual(other: Any) = other.isInstanceOf[UnitToBaseAssociation]
+    final override val kind = Kind.UnitToBaseAssociation //defaults to behave like an immutable Java HashMap<Unit, Base>
+    override def content: Map[Unit, Base]                //if isInsertOrdered is true, instance will be of ListMap
   }
 
-  trait BaseGroup extends Base {
-    final override val kind = Kind.BaseGroup  //defaults to behave like an immutable Java HashSet<GcnObject>, but implemented like immutable HashMap<Integer, GcnObject> where Integer is ranged 0..(size - 1)
-    val content: Seq[Base]
-    val toUnitToBaseAssociation: UnitToBaseAssociation
+  trait BaseGroup extends Base {                         //defaults to behave like an immutable Java HashSet<Base>, but implemented like immutable HashMap<Unit.Integer32, Base> where Integer is ranged 0..(size - 1)
+    final override def canEqual(other: Any) = other.isInstanceOf[BaseGroup]
+    final override val kind = Kind.BaseGroup
+    override def content: Seq[Base]
+    final def canBeUnitToBaseAssociationKeySet: Boolean = (
+           (kindShallow == Kind.Unit)
+        && (!isEmpty)
+        && (!hasEmptys)
+        && (!hasDuplicates)
+      )
+    def toUnitToBaseAssociation: UnitToBaseAssociation   //convenience conversion to UnitToBaseAssociation
   }
   
   object UnitType extends Enumeration {
-    type ConcreteType = Value                          //strongly influenced by Java types
-    val String =  Value("String")                      //String
-    val Boolean = Value("Boolean")                     //String with the possible exact (ignoring case) values of {"true", "false"}
-    val Trinary = Value("Trinary")                     //String with the possible exact (ignoring case) values of {"white", "grey", "black"}
-    val Identifier =  Value("Identifier")              //String starting with character from [A..Z||a..Z] and followed only by characters [A..Z||a..z||0..9||_]
-    val IntegerSigned32 = Value("IntegerSigned32")     //int
-    val IntegerSigned64 = Value("IntegerSigned64")     //long
-    val IntegerBig = Value("IntegerBig")               //BigInteger
-    val Decimal32 = Value("Decimal32")                 //float      (including "NaN")
-    val Decimal64 = Value("Decimal64")                 //double     (including "NaN")
-    val DecimalBig = Value("DecimalBig")               //BigDecimal (including "NaN")
-    val DateTimeInMillis = Value("DateTimeinMillis")   //(IntegerSigned64, String) (based on ISO8601 standard 0 based at "1970-01-01T00:00:00.000ZZ", time zone id) - per JodaTime
-    val DateTimeString = Value("DateTimeString")       //(String, String) (A string of the precise format derived from ISO8601 standard of "YYYY-MM-DDTHH:MM:SS.SSSZZZ", time zone id) - per JodaTime
-    val Currency32 = Value("Currency32")               //(String, int, int) =>        (name, value, number of digits behind the decimal point) example for USD $100 is ("USD", 10000, 2)
-    val Currency64 = Value("Currency64")               //(String, long, int) =>       "
-    val CurrencyBig = Value("CurrencyBig")             //(String, BigInteger, int) => "
-    val LongitudeLatitude = Value("LongitudeLatitude") //(long, int) => (value, number of digits behind the decimal point)
+    type ConcreteType = Value                            //strongly influenced by Java types
+    val String =  Value("String")                        //String
+    val Boolean = Value("Boolean")                       //String with the possible exact (ignoring case) values of {"true", "false"}
+    val Trinary = Value("Trinary")                       //String with the possible exact (ignoring case) values of {"white", "grey", "black"}
+    val Identifier =  Value("Identifier")                //String starting with character from [A..Z||a..Z] and followed only by characters [A..Z||a..z||0..9||_]
+    val IntegerSigned32 = Value("IntegerSigned32")       //int
+    val IntegerSigned64 = Value("IntegerSigned64")       //long
+    val IntegerBig = Value("IntegerBig")                 //BigInteger
+    val Decimal32 = Value("Decimal32")                   //float      (including "NaN")
+    val Decimal64 = Value("Decimal64")                   //double     (including "NaN")
+    val DecimalBig = Value("DecimalBig")                 //BigDecimal (including "NaN")
+    val DateTimeInMillis = Value("DateTimeinMillis")     //(IntegerSigned64, String) (based on ISO8601 standard 0 based at "1970-01-01T00:00:00.000ZZ", time zone id) - per JodaTime
+    val DateTimeString = Value("DateTimeString")         //(String, String) (A string of the precise format derived from ISO8601 standard of "YYYY-MM-DDTHH:MM:SS.SSSZZZ", time zone id) - per JodaTime
+    val Currency32 = Value("Currency32")                 //(String, int, int) =>        (name, value, number of digits behind the decimal point) example for USD $100 is ("USD", 10000, 2)
+    val Currency64 = Value("Currency64")                 //(String, long, int) =>       "
+    val CurrencyBig = Value("CurrencyBig")               //(String, BigInteger, int) => "
+    val LongitudeLatitude = Value("LongitudeLatitude")   //(long, int) => (value, number of digits behind the decimal point)
     //TODO: add other types here 
   }
   
-  trait Unit extends Base {
-    final override val kind = Kind.Unit  //defaults to behave like an immutable Java GcnObject, but implemented like immutable HashMap<Integer, GcnObject> where Integer is fixed at 0
+  trait Unit extends Base {                              //defaults to behave like an immutable Java Base, but implemented like immutable HashMap<Unit.IntegerSigned32, Base> where IntegerSigned32 is fixed at 0
+    final override def canEqual(other: Any) = other.isInstanceOf[Unit]
+    final override val kind = Kind.Unit
+    override def content: String
+    final override val isInsertOrdered = false
     final override val kindShallow = kind
+    //final override val isEmpty = false
+    final override val hasEmptys = false
+    final override val hasDuplicates = false
+    final override val isImpureShallow = false
+    final override val isImpureDeep = false
     val concreteType: UnitType.Value
-    val toBaseGroup: BaseGroup
+    def toBaseGroup: BaseGroup                           //convenience conversion to BaseGroup
   }
 }
